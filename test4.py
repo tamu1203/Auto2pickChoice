f9919e7bd022a6df20f9003fbde202d65cc9e98d
tamu1203 <quintettfoyer@gmail.com>
2022-02-13 21:44:33 +0900

    2pick選択画面から4つのカードを切り取り、カードプール(例ではエルフ+ニュートラル)の中で最もmaxValが大きいものを表示すした

--- test4.py
+++ test4.py
@@ -1,13 +1,24 @@
 import cv2
 import json
+import numpy as np
 from glob import glob
 
-pick_screen = cv2.imread('pictures/pick.jpg')
+screenshot = cv2.imread('pictures/pick.jpg')
+pick_screens = [None, None, None, None]
+pick_screens[0] = screenshot[200:700, 0:500]
+pick_screens[1] = screenshot[300:800, 300:800]
+pick_screens[2] = screenshot[300:800, 1100:1600]
+pick_screens[3] = screenshot[200:700, 1500:2000]
+# for pick_screen in pick_screens:
+# cv2.imshow('', pick_screen)
+# cv2.waitKey(0)
+# exit()
 # エルフだった場合の処理
 latest_pack = 123
 craft_path = 'E/'
 neutral_path = 'Ne/'
 img_card_pool = {}
+HEIGHT = 270
 
 json_open = open('json/all.json', 'r', encoding='utf-8')
 json_cards = json.load(json_open)
@@ -22,35 +33,42 @@ for pack in range(latest_pack, latest_pack-4, -1):
         glob(pack_path+neutral_path+'*')
     for img_path in imgs_path:
         img_card = cv2.imread(img_path)
-        img_card_pool[img_path[-13:-4]] = img_card[170:560, 110:420]
+        img = img_card[170:560, 110:420]
+        h, w = img.shape[:2]
+        width = round(w * (HEIGHT / h))
+        img_card_pool[img_path[-13:-4]
+                      ] = cv2.resize(img, dsize=(width, HEIGHT))
 
 # ベーシックカードをカードプールに追加
-imgs_path = glob('pictures/cards/100/'+craft_path+'*') + \
+img_paths = glob('pictures/cards/100/'+craft_path+'*') + \
     glob('pictures/cards/100/'+neutral_path+'*')
-for img_path in imgs_path:
+for img_path in img_paths:
     img_card = cv2.imread(img_path)
-    img_card_pool[img_path[-13:-4]] = img_card[170:560, 110:420]
+    img = img_card[170:560, 110:420]
+    h, w = img.shape[:2]
+    width = round(w * (HEIGHT / h))
+    img_card_pool[img_path[-13:-4]] = cv2.resize(img, dsize=(width, HEIGHT))
 
-
-# print(img_card_pool)
-# exit()
-i=0
-for key, value in img_card_pool.items():
-    result = cv2.matchTemplate(pick_screen, value, cv2.TM_CCORR_NORMED)
-    minVal, maxVal, minLoc, maxLoc = cv2.minMaxLoc(result)
-    if maxVal > 0.8:
-        i+=1
-        # for card in cards:
-        #     com = str(card['card_id'])
-        #     if key == com:
-        #         print(card['card_name'])
-        #         cv2.imshow(key,value)
-        #         cv2.waitKey(0)
-        # tl = maxLoc[0], maxLoc[1]
-        # br = maxLoc[0] + value.shape[1], maxLoc[1] + \
-        #     value.shape[0]
-        # dst = pick_screen.copy()
-        # cv2.rectangle(dst, tl, br, color=(0, 255, 0), thickness=2)
-        # cv2.imshow(key, dst)
-        # cv2.waitKey(0)
-print(i)
+for pick_screen in pick_screens:
+    maxMaxVal = 0
+    maxMaxLoc = []
+    for cardId, value in img_card_pool.items():
+        result = cv2.matchTemplate(pick_screen, value, cv2.TM_CCORR_NORMED)
+        _, maxVal, _, maxLoc = cv2.minMaxLoc(result)
+        if maxVal > maxMaxVal:
+            maxMaxVal = maxVal
+            maxMaxLoc = maxLoc
+            for card in cards:
+                if cardId == str(card['card_id']):
+                    card_name = card['card_name']
+                    print(card_name)
+    tl = maxMaxLoc[0], maxMaxLoc[1]
+    br = maxMaxLoc[0] + value.shape[1], maxMaxLoc[1] + \
+        value.shape[0]
+    dst = pick_screen.copy()
+    cv2.rectangle(dst, tl, br, color=(0, 255, 0), thickness=2)
+    # cv2.namedWindow('', cv2.WINDOW_NORMAL)
+    # card_name = card_name.encode("shift-jis").decode("utf-8", errors="ignore")
+    cv2.imshow(card_name, dst)
+    cv2.moveWindow(card_name, 0, 0)
+    cv2.waitKey(0)
