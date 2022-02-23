be4eafb710f76ac07585408c3b64a44d7cb6a795
tamu1203 <quintettfoyer@gmail.com>
2022-02-24 03:09:41 +0900

    gamewithの2pick得点サイトのスクレイピング。カード名をキー、得点をバリューの辞書を返却

---
+++ dl_card_scores.py
@@ -0,0 +1,46 @@
+import time
+import requests
+from bs4 import BeautifulSoup
+
+
+def dl_card_scores():
+    forest = 'https://shadowverse.gamewith.jp/article/show/36076'
+    sword = 'https://shadowverse.gamewith.jp/article/show/36082'
+    rune = 'https://shadowverse.gamewith.jp/article/show/36081'
+    dragon = 'https://shadowverse.gamewith.jp/article/show/36080'
+    shadow = 'https://shadowverse.gamewith.jp/article/show/36079'
+    blood = 'https://shadowverse.gamewith.jp/article/show/36078'
+    haven = 'https://shadowverse.gamewith.jp/article/show/36077'
+    portal = 'https://shadowverse.gamewith.jp/article/show/83018'
+    urls = [forest, sword, rune, dragon, shadow, blood, haven, portal]
+    card_scores = {}
+    for url in urls:
+        r = requests.get(url)
+        soup = BeautifulSoup(r.text, 'html.parser')
+
+        tables = soup.find_all('table', class_='sorttable')
+        rows = []
+        for table in tables:
+            rows.extend(table.find_all('tr'))
+
+        for row in rows:
+            dictRow = []
+            for cell in row.find_all(['td', 'th']):
+                dictRow.append(cell.get_text())
+            if dictRow[0] != '名前\n':
+                try:
+                    card_score = float(dictRow[2].replace('点', ''))
+                except ValueError:
+                    continue
+                card_name = dictRow[0].replace('\n', '')
+                card_scores[card_name] = card_score
+        time.sleep(1)
+    return card_scores
+
+
+def main():
+    print(dl_card_scores())
+
+
+if __name__ == '__main__':
+    main()
